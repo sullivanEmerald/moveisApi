52fd2ebf51432690fc5cd473e9e3792c512e51bf
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-06-03 01:22:59 +0100

    adding cors

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.querySelector('button').addEventListener('click', getData)
+
+async function getData(){
+    try{
+        const movie = document.querySelector('input').value
+
+        const res =  await fetch(`https://simple-moviestitle-api.herokuapp.com/api/merlin`)
+        const data = await res.json()
+
+        console.log(data)
+    }catch(error){
+        console.error(error)
+    }
+    
+
+}
